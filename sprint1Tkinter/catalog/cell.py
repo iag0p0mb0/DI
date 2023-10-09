1467f6ade7880a7194d6f7b3dcf885d9bda916fa
Iago <ipombof22@fpcoruna.afundacion.org>
2023-10-09 17:48:00 +0200

    Ventana detallada de cada animal añadida

--- sprint1Tkinter/catalog/cell.py
+++ sprint1Tkinter/catalog/cell.py
@@ -1,8 +1,11 @@
 import tkinter as tk
-from PIL import ImageTk
+from PIL import ImageTk, Image
 
 class Cell:
-    def __init__(self,title,path):
+    def __init__(self,title,path, desc):
         self.title = title
         self.path = path
-        self.image_tk = ImageTk.PhotoImage(file = self.path)
+        img = Image.open(self.path)# Con esto lo que hacemos es abrir la imagen de la posición en la que estemos, es decir, con este for vamos a pasar por todas las imagenes en el self.cell
+        img1 = img.resize((100, 100), Image.LANCZOS)# Aquí lo que hacemos es reescalar la imagen a 100 x 100
+        self.image_tk = ImageTk.PhotoImage(img1)
+        self.desc = desc
--- sprint1Tkinter/catalog/main.py
+++ sprint1Tkinter/catalog/main.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     root = Tk()
     app = MainWindow(root)
     root.mainloop()
+    

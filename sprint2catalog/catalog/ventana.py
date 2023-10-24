a880631be5755806d03152b5195f4e864c3e1526
Iago <ipombof22@fpcoruna.afundacion.org>
2023-10-24 09:43:53 +0200

    Ejercicio 7 completado

--- sprint2catalog/catalog/ventana.py
+++ sprint2catalog/catalog/ventana.py
@@ -7,8 +7,10 @@ from detail_window import detailWindow
 
 class MainWindow():
         
-    def onButtonClicked(self):
-        #detailWindow(cell)
+    def onButtonClickedDetailWindow(self, cell):
+        detailWindow(cell)
+
+    def onButtonClickedAcercaDe(self):
         messagebox.showinfo("Acerca del desarrollador", "Nombre: Iago Pombo\nEdad: 19")  
 
     def __init__(self, root, json_data):
@@ -30,29 +32,52 @@ class MainWindow():
             self.cells.append(cell)
 
         # ancho y alto iguales para hacer la ventana cuadrada:
-        widthWindow = 200
-        heightWindow = 450
-        self.root.geometry(f"{int(widthWindow)}x{int(heightWindow)}")
+        size = 200
+        self.root.geometry(f"{int(size)}x{int(size)}")
         # si implementamos este código, ancho y alto de la ventana serán números accesibles por winfo:
-        self.root.config(width=widthWindow, height=heightWindow)
+        self.root.config(width=size, height=size)
         x = (self.root.winfo_screenwidth() - self.root.winfo_reqwidth())/2
         y = (self.root.winfo_screenheight() - self.root.winfo_reqheight())/2
         self.root.geometry(f"+{int(x)}+{int(y)}")
-
-        #recorremos la lista de celdas para que se muestran en la ventana principal
-        for i, cell in enumerate (self.cells):
-            label = ttk.Label(root, image = cell.img, text = cell.name, compound = tk.BOTTOM)
-            label.grid(row = i, column = 0)
-            label.bind("<Button-1>", lambda event, cell = cell: self.onButtonClicked(cell))
 
         # Crear una barra de menús
         barra_menus = tk.Menu()
         menu_ayuda = tk.Menu(barra_menus, tearoff=False)
         # Agregarlo a la barra
         menu_ayuda.add_command(
-        label="Acerca de",
-        command=self.onButtonClicked
+        label="Ayuda",
+        command=self.onButtonClickedAcercaDe
         )
-        barra_menus.add_cascade(menu=menu_ayuda, label="Acerca de")
+        barra_menus.add_cascade(menu=menu_ayuda, label="Ayuda")
         root.config(menu=barra_menus)  
-        root.mainloop()
+
+        # implementación scrollbar:
+        self.canvas = tk.Canvas(root, highlightthickness=0)
+        self.scrollbar = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview, width=15)
+        self.scrollable_frame = tk.Frame(self.canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+
+        self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        #recorremos la lista de celdas para que se muestran en la ventana principal
+        for i, cell in enumerate (self.cells):
+            frame = tk.Frame(self.scrollable_frame)
+            frame.pack(pady=10)
+
+            label = ttk.Label(frame, image = cell.img, text = cell.name, compound = tk.BOTTOM)
+            label.grid(row = i, column = 0)
+            label.bind("<Button-1>", lambda event, cell = cell: self.onButtonClickedDetailWindow(cell))
+
+
+        self.canvas.grid(row = 0, column = 0, sticky="nsew")
+        self.scrollbar.grid(row = 0, column = 1, sticky="ns")
+
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)

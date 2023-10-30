0f30e8fcb4c9d4bb349196fd8300c6e8a26a191b
Iago <ipombof22@fpcoruna.afundacion.org>
2023-10-30 17:50:00 +0100

    Añadido botón y viculado a clase java

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -2,13 +2,26 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
-
+    private Button navegarAlDetalleButton;
+    private Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+        navegarAlDetalleButton = findViewById(R.id.navegarAlDetalle);//inicializamos el botón del xml
+        navegarAlDetalleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);//Creamos un intent para poder nombrarlo en la función de StartActivity
+                startActivity(intent);
+            }
+        });
     }
 }
